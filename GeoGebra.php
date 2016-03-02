c089e5da5c72ca2c5a8bf6a78e09947f112bb01f
Jean-Frederic <jeanfrederic.wiki@gmail.com>
2016-03-02 22:25:42 +0000

    Add PHP linting with mediawiki-codesniffer
    * Add MediaWiki CodeSniffer to composer.json
    * Disable PHPCS rules currently failing rules in phpcs.xml
    * Fix CodeSniffer errors automatically using phpcs diff report [1]
    
    [1] The following errors were automatically fixed:
    - Generic.ControlStructures.InlineControlStructure.NotAllowed
    - Generic.Files.LineLength.TooLong
    - Generic.Functions.FunctionCallArgumentSpacing.NoSpaceAfterComma
    - Generic.Functions.OpeningFunctionBraceKernighanRitchie.BraceOnNewLine
    - Generic.Functions.OpeningFunctionBraceKernighanRitchie.SpaceAfterBracket
    - Generic.PHP.LowerCaseKeyword.Found
    - Generic.WhiteSpace.DisallowSpaceIndent.SpacesUsed
    - MediaWiki.Usage.DirUsage.FunctionFound
    - MediaWiki.VariableAnalysis.UnusedGlobalVariables
    - MediaWiki.WhiteSpace.SpaceAfterControlStructure.Incorrect
    - MediaWiki.WhiteSpace.SpaceyParenthesis.SingleSpaceAfterOpenParenthesis
    - MediaWiki.WhiteSpace.SpaceyParenthesis.SingleSpaceBeforeCloseParenthesis
    - PSR2.Classes.PropertyDeclaration.ScopeMissing
    - Squiz.WhiteSpace.ScopeClosingBrace.Indent
    - Squiz.WhiteSpace.SuperfluousWhitespace.EmptyLines
    - Squiz.WhiteSpace.SuperfluousWhitespace.EndLine
    
    Change-Id: I03265fadb0dc7d4ad3c9247dc17ee45884de6452

--- GeoGebra.php
+++ GeoGebra.php
@@ -10,12 +10,12 @@ $wgExtensionCredits['parserhook'][] = array(
 	'name' => 'GeoGebra',
 	'version' => '3.0.7',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:GeoGebra',
-	'author' => array( 'Rudolf Grossmann','Zbynek Konecny'),
+	'author' => array( 'Rudolf Grossmann','Zbynek Konecny' ),
 	'descriptionmsg' => 'geogebra-desc',
 );
 
-$wgAutoloadClasses['ExtGeoGebra'] = dirname( __FILE__ ) . '/GeoGebra.body.php';
-$wgExtensionMessagesFiles['GeoGebra'] = dirname( __FILE__ ) . '/GeoGebra.i18n.php';
+$wgAutoloadClasses['ExtGeoGebra'] = __DIR__ . '/GeoGebra.body.php';
+$wgExtensionMessagesFiles['GeoGebra'] = __DIR__ . '/GeoGebra.i18n.php';
 $wgMessagesDirs['GeoGebra'] = __DIR__ . '/i18n';
 
 $wgHooks['ParserFirstCallInit'][] = 'wfGeoGebraInit';
@@ -26,6 +26,6 @@ $wgHooks['BeforePageDisplay'][] = 'ExtGeoGebra::injectJS';
  * @return bool
  */
 function wfGeoGebraInit( $parser ) {
-	$parser->setHook('ggb_applet','ExtGeoGebra::geogebraTag');
+	$parser->setHook( 'ggb_applet', 'ExtGeoGebra::geogebraTag' );
 	return true;
 }
